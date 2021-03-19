29d38f4e4d8e4c88a6b015b13dffa1259a33d212
Automated Version Bump <gh-action-bump-version@users.noreply.github.com>
2021-03-19 20:40:47 +0000

    ci: version bump to v1.1.5

--- playground/src/component-lib/useLocalStorageReducer/useLocalStorageReducer.d.ts
+++ playground/src/component-lib/useLocalStorageReducer/useLocalStorageReducer.d.ts
@@ -1,9 +1,4 @@
 import { Reducer, ReducerState, Dispatch, ReducerAction } from "react";
-export declare function useLocalStorageReducer<I, A>(
-  key: string,
-  reducer: Reducer<I, A>,
-  initialState: I,
-  expire?: number | boolean
-): [ReducerState<Reducer<I, A>>, Dispatch<ReducerAction<Reducer<I, A>>>];
+export declare function useLocalStorageReducer<I, A>(key: string, reducer: Reducer<I, A>, initialState: I, expire?: number | boolean): [ReducerState<Reducer<I, A>>, Dispatch<ReducerAction<Reducer<I, A>>>];
 export default useLocalStorageReducer;
 //# sourceMappingURL=useLocalStorageReducer.d.ts.map
